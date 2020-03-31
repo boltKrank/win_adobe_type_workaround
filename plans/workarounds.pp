a0c2416c275675c1cde4db8bb2164c3fb2a7c745
boltKrank <simon.anderson111@gmail.com>
2020-03-31 11:02:00 +1100

    Delete workarounds.pp

--- plans/workarounds.pp
+++
@@ -1,9 +0,0 @@
-plan win_adobe_type_workaround::workarounds(
-  Boolean $rollback = false,
-) {
-
-  # process workarounds
-  run_task('win_adobe_type_workaround::disable_preview_details_panes', rollback => $rollback)
-  run_task('win_adobe_type_workaround::disable_webclient', rollback => $rollback)
-  
-}
